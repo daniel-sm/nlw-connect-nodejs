24ba87ed4809d9bcfdf9f502f371ab6ba7bc2778
Daniel <danieldsm.on@gmail.com>
2025-02-18 20:59:17 -0300

    configure cors, validator and serializer

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,16 @@
 import { fastify } from "fastify";
+import { fastifyCors } from "@fastify/cors"
+import {
+  validatorCompiler, 
+  serializerCompiler,
+} from "fastify-type-provider-zod"
 
 const app = fastify()
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifyCors)
 
 app.get("/hello", () => "Hello World!")
 
