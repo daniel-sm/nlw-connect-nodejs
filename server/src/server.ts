1cf3ad62a1194b36a10728630dd7099d049bb07a
Daniel Santos Maciel <daniel.maciel@gftpromotora.com.br>
2025-02-19 15:53:59 -0300

    configure swagger

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,8 +4,11 @@ import {
   validatorCompiler, 
   serializerCompiler,
   type ZodTypeProvider,
+  jsonSchemaTransform,
 } from "fastify-type-provider-zod"
-import { z } from "zod"
+import { fastifySwagger } from "@fastify/swagger"
+import { fastifySwaggerUi } from "@fastify/swagger-ui"
+import { subscribeToEventRoute } from "./routes/subscribe-to-event-route";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -14,29 +17,21 @@ app.setValidatorCompiler(validatorCompiler)
 
 app.register(fastifyCors)
 
-app.post("/subscriptions", {
-  schema: {
-    body: z.object({
-      name: z.string(),
-      email: z.string().email(),
-    }),
-    response: {
-      201: z.object({
-        name: z.string(),
-        email: z.string(),
-      })
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "NLW Connect",
+      version: "0.0.1",
     }
-  }
-}, async (request, reply) => {
-  const { name, email } = request.body
-
-  // criacao da inscricao em banco de dados
-  
-  return reply.status(201).send({
-    name,
-    email,
-  })
+  },
+  transform: jsonSchemaTransform,
 })
+
+app.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
+})
+
+app.register(subscribeToEventRoute)
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running...");
