e42dd16abf332e256d760bb2c79f91f610f77e81
Daniel Santos Maciel <daniel.maciel@gftpromotora.com.br>
2025-02-19 15:05:49 -0300

    create first route

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,16 +3,40 @@ import { fastifyCors } from "@fastify/cors"
 import {
   validatorCompiler, 
   serializerCompiler,
+  type ZodTypeProvider,
 } from "fastify-type-provider-zod"
+import { z } from "zod"
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 
 app.register(fastifyCors)
 
-app.get("/hello", () => "Hello World!")
+app.post("/subscriptions", {
+  schema: {
+    body: z.object({
+      name: z.string(),
+      email: z.string().email(),
+    }),
+    response: {
+      201: z.object({
+        name: z.string(),
+        email: z.string(),
+      })
+    }
+  }
+}, async (request, reply) => {
+  const { name, email } = request.body
+
+  // criacao da inscricao em banco de dados
+  
+  return reply.status(201).send({
+    name,
+    email,
+  })
+})
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running...");
