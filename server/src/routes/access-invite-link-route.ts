b8f2d52e5ff5c5c91ccfab70672ac60f6cb010db
Daniel <danieldsm.on@gmail.com>
2025-02-20 12:07:08 -0300

    create invites route

---
+++ server/src/routes/access-invite-link-route.ts
@@ -0,0 +1,38 @@
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { env } from '../env'
+import { accessInviteLink } from '../functions/acess-invite-link'
+
+export const accessInviteLinkRoute: FastifyPluginAsyncZod = async app => {
+  app.get(
+    '/invites/:subscriberId',
+    {
+      schema: {
+        summary: 'Invite link and redirects user',
+        tags: ['referral'],
+        params: z.object({
+          subscriberId: z.string(),
+        }),
+        response: {
+          201: z.object({
+            subscriberId: z.string(),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { subscriberId } = request.params
+
+      await accessInviteLink({ subscriberId })
+
+      const redirectURL = new URL(env.WEB_URL)
+
+      redirectURL.searchParams.set('referrer', subscriberId)
+
+      // 301: redirect permanente
+      // 302: redirect temporario
+
+      return reply.redirect(redirectURL.toString(), 302)
+    }
+  )
+}
